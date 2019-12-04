4611eddc645b505e2ab90f3142634a0dc82df9e6
40823251 <40823251@gm.nfu.edu.tw>
2019-12-04 21:49:17 +0800

    add temp2

--- downloads/dart_ex/temp1/temp1.dart
+++ downloads/dart_ex/temp1/temp1.dart
@@ -1,12 +1,24 @@
-// 定義一個 C to F function
-cnfu(num c){
+/*
+將 Dart 程式存在倉儲中特定目錄下, 然後以 Dartpad.github.io 呼叫的好處是, 程式開發者可以隨時在 Github 網際環境中編輯改版程式, 之後在近端改版時, 
+可以將程式以 git pull 的方式拉到近端合併.
+*/
+// 將華氏溫度轉為攝氏溫度的公式: celsius = 5/9 ( fahrenheit − 32)
+// 定義一個 celsius 轉 fahrenheit  函式
+// 輸入參數 c 資料型別為 num, 表示可以是整數或浮點數
+// Dart 的整數資料型別為 int: https://api.dartlang.org/stable/2.6.1/dart-core/int-class.html
+// Dart 的浮點數資料型別為 double: https://api.dartlang.org/stable/2.6.1/dart-core/double-class.html
+CtoF(num c){
+// 將輸入的攝氏溫度參數 C 數值乘上 9 再除以 5 之後, 再加上 32, 可以得到華氏溫度
 return c*9/5 + 32;
 }
-// define a F to C function
-fnfu(num f){
+
+// 定義一個 fahrenheit 轉 celsius  函式
+FtoC(num f){
+// 將輸入的華氏溫度參數減 32 之後, 乘上 5 再除以 9 之後, 可以得到攝氏溫度
 return (f - 32)*5/9;
 }
-// 每一個 Dart 程式都從 main() 開始執行
+
+// 每一個 Dart 程式都必須從 main() 開始執行
 main() {
   // 宣告 len 整數變數, 準備設為各字串的長度
   int len;
@@ -25,6 +37,7 @@ main() {
     //print(data[len-1]);
     type = data[len-1];
     // https://api.dartlang.org/stable/2.6.1/dart-core/Object/runtimeType.html
+    // 希望取得 Dart 程式中特定物件的資料型別, 可以透過該物件的 runtimeType 屬性取得
     //print("目前 type 變數資料型別為: ${type.runtimeType}");
     // 根據溫度類別轉給對應函式進行溫度轉換
     // https://api.dartlang.org/stable/2.6.1/dart-core/String/substring.html
@@ -39,9 +52,11 @@ main() {
     // 之後就可以根據 type 與 number 執行各數列元素的溫度轉換
     
     if (type == "C"){
-      print("攝氏 $number° = 華氏 ${cnfu(number)}°");
+      //print("C: $type, $number");
+      print("攝氏 $number 度 = 華氏 ${CtoF(number)} 度");
     }else{
-      print("華氏 $number° = 攝氏 ${fnfu(number)}°");
+      //print("F: $type, $number");
+      print("華氏 $number 度 = 攝氏 ${FtoC(number)} 度");
     }
       
   } // for
